062773ed58c4b9f626d964c48978a4ba99ffa319
segundara <segundara@gmail.com>
2020-09-21 23:19:03 +0300

    update index file in course_register route

--- src/routes/course_register/index.js
+++ src/routes/course_register/index.js
@@ -1,37 +1,77 @@
 const express = require("express")
 const db = require("../../db")
+const sgMail = require("@sendgrid/mail")
 
 const registerRouter = express.Router()
 
 registerRouter.post("/", async (req, res) => {
+    const checkRegister = await db.query(`SELECT _id FROM course_register 
+                                      WHERE courseid = $1 AND studentid = $2`,
+        [req.body.courseid, req.body.studentid])
+
+    if (checkRegister.rowCount > 0)
+        return res.send("You have already registered for this course!")
+
     const response = await db.query("INSERT INTO course_register (studentid, courseid, reg_date) VALUES ($1, $2, $3) RETURNING _id",
         [req.body.studentid, req.body.courseid, req.body.reg_date])
+
+    const record = await db.query(`SELECT courses._id, courses.name, courses.description, courses.semester, course_register.reg_date
+        FROM course_register JOIN "courses" ON course_register.courseid = "courses"._id
+        WHERE course_register._id = $1
+        GROUP BY courses._id, courses.name, courses.description, courses.semester, course_register.reg_date
+        `, [response.rows[0]._id])
+
+    const studentInfo = await db.query(`SELECT students._id, students.firstname, students.lastname, students.email
+        FROM course_register JOIN "students" ON course_register.studentid = "students"._id
+        WHERE studentid = $1
+        GROUP BY students._id, students.firstname, students.lastname, students.email
+        `, [req.body.studentid])
+
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    const msg = {
+        to: studentInfo.rows[0].email,
+        from: 'srms@school.com',
+        subject: 'Course Registration Successful',
+        text: `Hello ${studentInfo.rows[0].firstname} ${studentInfo.rows[0].lastname}, 
+                \nYour registration for ${record.rows[0].name} which is scheduled for ${record.rows[0].semester} semester has been saved.`
+    };
+    await sgMail.send(msg);
 
     res.send(response.rows[0])
 })
 
-registerRouter.get("/:studentid", async (req, res) => {
+registerRouter.get("/course_list/:studentid", async (req, res) => {
     const response = await db.query(`SELECT courses._id, courses.name, courses.description, courses.semester, course_register.reg_date
                                      FROM course_register JOIN "courses" ON course_register.courseid = "courses"._id
                                      WHERE studentid = $1
                                      GROUP BY courses._id, courses.name, courses.description, courses.semester, course_register.reg_date
                                      `, [req.params.studentid])
-
+    console.log(response.rows)
     res.send({ count: response.rows.length, data: response.rows })
 })
 
-registerRouter.delete("/:studentid/:id", async (req, res) => {
+registerRouter.get("/student_list/:courseid", async (req, res) => {
+    const response = await db.query(`SELECT students._id, students.firstname, students.lastname, students.email, course_register.reg_date
+                                     FROM course_register JOIN "students" ON course_register.studentid = "students"._id
+                                     WHERE courseid = $1
+                                     GROUP BY students._id, students.firstname, students.lastname, students.email, course_register.reg_date
+                                     `, [req.params.courseid])
+    console.log(response.rows)
+    res.send({ count: response.rows.length, data: response.rows })
+})
+
+registerRouter.delete("/:studentid/:courseid", async (req, res) => {
 
     const response = await db.query(`DELETE FROM course_register where _id IN
                                      (SELECT _id FROM course_register 
                                       WHERE courseid = $1 AND studentid = $2
                                       LIMIT 1)`,
-        [req.params.id, req.params.studentid])
+        [req.params.courseid, req.params.studentid])
 
     if (response.rowCount === 0)
         return res.status(404).send("Not found")
 
-    res.send("DELETED")
+    res.send("Course successfully removed from your list of registered courses!")
 })
 
 
