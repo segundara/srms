70906dcf9ceb00ccf8161047311daf3eff63b431
segundara <segundara@gmail.com>
2020-09-25 02:54:42 +0300

    update to authorize middleware file

--- src/routes/middlewares/authorize.js
+++ src/routes/middlewares/authorize.js
@@ -1,11 +1,12 @@
 const jwt = require("jsonwebtoken")
-const { verifyJWT } = require("../students/authTools")
+const { verifyStudentJWT } = require("../students/auth_students")
+const { verifyLecturerJWT } = require("../lecturers/auth_lecturers")
 const db = require("../../db")
 
-const authorize = async (req, res, next) => {
+const authorizeStudent = async (req, res, next) => {
   try {
     const token = req.header("Authorization").replace("Bearer ", "")
-    const decoded = await verifyJWT(token)
+    const decoded = await verifyStudentJWT(token)
     const user = await db.query('SELECT * FROM "students" WHERE _id= $1',
       [decoded._id])
 
@@ -29,4 +30,31 @@ const authorize = async (req, res, next) => {
   }
 }
 
-module.exports = authorize
+const authorizeLecturer = async (req, res, next) => {
+  try {
+    const token = req.header("Authorization").replace("Bearer ", "")
+    const decoded = await verifyLecturerJWT(token)
+    const user = await db.query('SELECT * FROM "lecturers" WHERE _id= $1',
+      [decoded._id])
+
+    if (!user) {
+      throw new Error()
+    }
+
+    req.token = token
+    req.user = user.rows[0]
+
+    if (req.user.token === '') {
+      const err = new Error("Sorry you need to login again!")
+      err.httpStatusCode = 401
+      next(err)
+    }
+    next()
+  } catch (e) {
+    const err = new Error("Please authenticate")
+    err.httpStatusCode = 401
+    next(err)
+  }
+}
+
+module.exports = { authorizeStudent, authorizeLecturer }
