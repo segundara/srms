f6e0e42ac58241a224d68e2a27039f1c8270680d
segundara <segundara@gmail.com>
2020-10-02 09:59:01 +0300

    updated index file in course_register route

--- src/routes/course_register/index.js
+++ src/routes/course_register/index.js
@@ -1,11 +1,12 @@
 const express = require("express")
 const db = require("../../db")
 const sgMail = require("@sendgrid/mail")
+const { authorize, forAllButStudent, onlyForAdmin } = require("../middlewares/authorize")
 
 const registerRouter = express.Router()
 
 // endpoint for course registration, needed by student
-registerRouter.post("/", async (req, res) => {
+registerRouter.post("/", authorize, async (req, res) => {
     const checkRegister = await db.query(`SELECT _id FROM course_register 
                                       WHERE courseid = $1 AND studentid = $2`,
         [req.body.courseid, req.body.studentid])
@@ -13,14 +14,14 @@ registerRouter.post("/", async (req, res) => {
     if (checkRegister.rowCount > 0)
         return res.send("You have already registered for this course!")
 
-    const response = await db.query("INSERT INTO course_register (studentid, courseid, reg_date) VALUES ($1, $2, $3) RETURNING _id",
+    const newCourseReg = await db.query("INSERT INTO course_register (studentid, courseid, reg_date) VALUES ($1, $2, $3) RETURNING _id",
         [req.body.studentid, req.body.courseid, req.body.reg_date])
 
     const record = await db.query(`SELECT courses._id, courses.name, courses.description, courses.semester, course_register.reg_date
         FROM course_register JOIN "courses" ON course_register.courseid = "courses"._id
         WHERE course_register._id = $1
         GROUP BY courses._id, courses.name, courses.description, courses.semester, course_register.reg_date
-        `, [response.rows[0]._id])
+        `, [newCourseReg.rows[0]._id])
 
     const studentInfo = await db.query(`SELECT students._id, students.firstname, students.lastname, students.email
         FROM course_register JOIN "students" ON course_register.studentid = "students"._id
@@ -38,11 +39,11 @@ registerRouter.post("/", async (req, res) => {
     };
     await sgMail.send(msg);
 
-    res.send(response.rows[0])
+    res.send(newCourseReg.rows[0])
 })
 
 // endpoint for viewing registered course list, needed by student
-registerRouter.get("/course_list/:studentid", async (req, res) => {
+registerRouter.get("/course_list/:studentid", authorize, async (req, res) => {
     const response = await db.query(`SELECT courses._id, courses.name, courses.description, courses.semester, course_register.reg_date
                                      FROM course_register JOIN "courses" ON course_register.courseid = "courses"._id
                                      WHERE studentid = $1
@@ -53,7 +54,7 @@ registerRouter.get("/course_list/:studentid", async (req, res) => {
 })
 
 // endpoint for viewing students list in a particular course, needed by lecturer
-registerRouter.get("/student_list/:courseid", async (req, res) => {
+registerRouter.get("/student_list/:courseid", authorize, forAllButStudent, async (req, res) => {
     const response = await db.query(`SELECT students._id, students.firstname, students.lastname, students.email, course_register.reg_date
                                      FROM course_register JOIN "students" ON course_register.studentid = "students"._id
                                      WHERE courseid = $1
@@ -64,7 +65,7 @@ registerRouter.get("/student_list/:courseid", async (req, res) => {
 })
 
 // endpoint for removing a course from registered list, needed by student
-registerRouter.delete("/:studentid/:courseid", async (req, res) => {
+registerRouter.delete("/:studentid/:courseid", authorize, async (req, res) => {
 
     const response = await db.query(`DELETE FROM course_register where _id IN
                                      (SELECT _id FROM course_register 
