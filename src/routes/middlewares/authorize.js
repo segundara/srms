449755422c70424119d63f6885cc88efbf7abf96
segundara <segundara@gmail.com>
2020-10-08 18:53:20 +0300

    update authorize middleware

--- src/routes/middlewares/authorize.js
+++ src/routes/middlewares/authorize.js
@@ -1,7 +1,3 @@
-const jwt = require("jsonwebtoken")
-// const { verifyStudentJWT } = require("../students/auth_students")
-// const { verifyLecturerJWT } = require("../lecturers/auth_lecturers")
-// const { verifyAdminJWT } = require("../admin/auth_admin")
 const { verifyJWT } = require("../users/auth_users")
 const db = require("../../db")
 
@@ -49,86 +45,5 @@ const forAllButStudent = async (req, res, next) => {
     next(err)
   }
 }
-
-// const authorizeStudent = async (req, res, next) => {
-//   try {
-//     const token = req.cookies.accessToken
-//     const decoded = await verifyStudentJWT(token)
-//     const user = await db.query('SELECT * FROM "students" WHERE _id= $1',
-//       [decoded._id])
-
-//     if (!user) {
-//       throw new Error()
-//     }
-
-//     req.token = token
-//     req.user = user.rows[0]
-
-//     if (req.user.token === null) {
-//       const err = new Error("Sorry you already logged out!")
-//       err.httpStatusCode = 403
-//       next(err)
-//     }
-//     next()
-//   } catch (e) {
-//     const err = new Error("Please authenticate")
-//     err.httpStatusCode = 401
-//     next(err)
-//   }
-// }
-
-// const authorizeLecturer = async (req, res, next) => {
-//   try {
-//     const token = req.cookies.accessToken
-//     const decoded = await verifyLecturerJWT(token)
-//     const user = await db.query('SELECT * FROM "lecturers" WHERE _id= $1',
-//       [decoded._id])
-
-//     if (!user) {
-//       throw new Error()
-//     }
-
-//     req.token = token
-//     req.user = user.rows[0]
-
-//     if (req.user.token === null) {
-//       const err = new Error("Sorry you already logged out!")
-//       err.httpStatusCode = 401
-//       next(err)
-//     }
-//     next()
-//   } catch (e) {
-//     const err = new Error("Please authenticate")
-//     err.httpStatusCode = 401
-//     next(err)
-//   }
-// }
-
-// const authorizeAdmin = async (req, res, next) => {
-//   try {
-//     const token = req.cookies.accessToken
-//     const decoded = await verifyAdminJWT(token)
-//     const user = await db.query('SELECT * FROM "admin" WHERE _id= $1',
-//       [decoded._id])
-
-//     if (!user) {
-//       throw new Error()
-//     }
-
-//     req.token = token
-//     req.user = user.rows[0]
-
-//     if (req.user.token === null) {
-//       const err = new Error("Sorry you already logged out!")
-//       err.httpStatusCode = 401
-//       next(err)
-//     }
-//     next()
-//   } catch (e) {
-//     const err = new Error("Please authenticate")
-//     err.httpStatusCode = 401
-//     next(err)
-//   }
-// }
 
 module.exports = { authorize, onlyForAdmin, forAllButStudent }
